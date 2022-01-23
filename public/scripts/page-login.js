bb4410f7873fbf921e14a6bb4b4907d2cb5f46f4
Ricardo Mateus <r.fmateus@hotmail.com>
2022-01-23 13:35:37 +0000

    Login and signup form

--- public/scripts/page-login.js
+++ public/scripts/page-login.js
@@ -20,19 +20,27 @@ function switchLoginSignup(changeTo) {
     `
   } else {
     authForm.innerHTML = `
-      <h1 class="form--title">Faz Login na tua conta</h1>
+      <h1 class="form--title">Faz o teu registo</h1>
       <div class="form--field">
-        <label for="loginEmail">Email</label>
-        <input id="loginEmail" type="email" name="email" value="">
+        <label for="signupName">Nome</label>
+        <input id="signupName" type="text" name="name" value="">
       </div>
       <div class="form--field">
-        <label for="loginPassword">Password</label>
-        <input id="loginPassword" type="password" name="password" value="">
+        <label for="signupEmail">Email</label>
+        <input id="signupEmail" type="email" name="email" value="">
       </div>
-      <input type="submit" name="submit" value="Login" class="btn btn--round">
+      <div class="form--field">
+        <label for="signupPassword">Password</label>
+        <input id="signupPassword" type="password" name="password" value="">
+      </div>
+      <div class="form--field__checkbox">
+        <input id="signupRGPD" type="checkbox" name="rgpd">
+        <label for="signupRGPD">RGPD - Termos e Condições</label>
+      </div>
+      <input type="submit" name="submit" value="Registar" class="btn btn--round">
       <span class="form--info">
-        Ainda não tens conta?
-        <a type="button" class="btn--text" onclick="switchLoginSignup('Login')">Já tens conta!</a>
+        Já tens conta?
+        <a type="button" class="btn--text" onclick="switchLoginSignup('Login')">Faz login aqui!</a>
       </span>
     `
   }
