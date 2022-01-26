76e697f490f38ecc7b0589c2b762eccfbea17c7a
Ricardo Mateus <r.fmateus@hotmail.com>
2022-01-26 02:14:31 +0000

    update

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,11 +1,17 @@
 import express from 'express';
-import { cardsPage, staffPage, userPage, updateCard } from '../controllers/user.js';
+import { bookingsPage, cardsPage, clientsPage, invoicesPage, sddPage, servicesPage, staffPage, userPage, usersPage, updateCard } from '../controllers/user.js';
 
 const userRouter = express.Router()
 
 userRouter.get('/', userPage)
+userRouter.get('/bookings', bookingsPage)
 userRouter.get('/cards', cardsPage)
+userRouter.get('/clients', clientsPage)
+userRouter.get('/invoices', invoicesPage)
+userRouter.get('/sdd', sddPage)
+userRouter.get('/services', servicesPage)
 userRouter.get('/staff', staffPage)
+userRouter.get('/users', usersPage)
 
 userRouter.post('/card', updateCard)
 
